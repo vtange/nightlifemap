e1a59ab45422834710f1b461706f0ef28f93cbe7
Victor Tang <victortang1992@gmail.com>
2016-02-04 14:07:58 -0800

    add angular

---
+++ public/app.js
@@ -0,0 +1,13 @@
+(function() {
+    //start of function
+  var app = angular.module('NightLifeMap', []);
+
+app.controller('MainCtrl', ['$scope', function($scope){
+
+	
+	
+	
+	
+}]);//end of controller
+  //end of function
+})();
